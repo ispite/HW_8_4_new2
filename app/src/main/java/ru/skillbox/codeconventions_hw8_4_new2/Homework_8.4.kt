06f31c7f1cb4fb4e9d3048262671445385631a81
Denis Solovyov <infinityx93@gmail.com>
2021-04-12 21:03:07 +0300

    semicolon

--- app/src/main/java/ru/skillbox/codeconventions_hw8_4_new2/Homework_8.4.kt
+++ app/src/main/java/ru/skillbox/codeconventions_hw8_4_new2/Homework_8.4.kt
@@ -3,10 +3,10 @@ package ru.skillbox.codeconventions_hw8_4_new2
 import classes.Wheel
 
 fun main() {
-    val wheel = Wheel()
-    wheel.catchCheck()
+    val wheel = Wheel();
+    wheel.catchCheck();
     wheel.catchPressureException(-2.0);
     wheel.catchPressureException(2.0);
-    wheel.catchPressureException(20.0)
+    wheel.catchPressureException(20.0);
     wheel.catchCheck();
 }
